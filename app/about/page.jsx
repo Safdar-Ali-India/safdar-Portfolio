01c7620756d83e0a3dd0c9c1c8b0eea4da60302c
Safdar-Ali-India <safdarali63166@gmail.com>
2024-06-19 04:32:45 +0530

    build error fixed

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -73,7 +73,7 @@ function page() {
           <div className="dark:text-neutral-300 font-InterMedium mt-3 text-sm lg:text-lg light:text-slate-800">
             <ul>
             <li> Created more tha 70+ coding lectures 👨‍💻 on YouTube </li>
-              <li>🛠 Made 20+ Projects Using MERN</li>
+              <li>🛠 Made 20+ Projects Using React and other CMS platforms</li>
               <li>💵 Made my first 140$ w/ Freelancing</li>
               <li>🌟 Hit 3K+ Followers on Blog / LinkedIn</li>
           

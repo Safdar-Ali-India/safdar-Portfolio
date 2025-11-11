3db658921eb1f6a7a8025873810ddb48749bfb8c
Safdar-Ali-India <safdarali63166@gmail.com>
2025-11-11 21:18:48 +0530

    refractor: code splitting and optimization done

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,56 +1,35 @@
-import React from "react";
+import React from "react"
 import Link from "next/link"
-import Image from 'next/image'
+import Image from "next/image"
 
 const NotFound = () => {
   return (
     <section className="bg-white dark:bg-gray-900 ">
       <div className="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12">
         <div className="wf-ull lg:w-1/2">
-          <p className="text-sm font-medium text-blue-500 dark:text-blue-400">
-            404 ERROR
-          </p>
-          <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">
-            Page not found
-          </h1>
-          <p className="mt-4 text-gray-500 dark:text-gray-400">
-            Sorry, the page you are looking for doesn&apos;t exist.
-          </p>
+          <p className="text-sm font-medium text-blue-500 dark:text-blue-400">404 ERROR</p>
+          <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">Page not found</h1>
+          <p className="mt-4 text-gray-500 dark:text-gray-400">Sorry, the page you are looking for doesn&apos;t exist.</p>
 
           <div className="flex items-center mt-6 gap-x-3">
             <button className="hover:text-black text-white flex items-center justify-center w-1/2 px-5 py-2 text-sm transition-colors duration-200 bg-blue-700 border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-5 h-5 rtl:rotate-180"
-              >
-                <path
-                  stroke-linecap="round"
-                  strokeLinejoin="round"
-                  d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
-                />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 rtl:rotate-180">
+                <path stroke-linecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
               </svg>
 
-              <span><Link href="/">Home</Link></span>
+              <span>
+                <Link href="/">Home</Link>
+              </span>
             </button>
           </div>
         </div>
 
         <div className="relative w-full mt-12 lg:w-1/2 lg:mt-0">
-          <Image
-            className="w-full max-w-lg lg:mx-auto"
-            src="https://merakiui.com/images/components/illustration.svg"
-            alt=""
-            height={550}
-            width={550}
-          />
+          <Image className="w-full max-w-lg lg:mx-auto" src="https://merakiui.com/images/components/illustration.svg" alt="" height={550} width={550} />
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default NotFound;
+export default NotFound
